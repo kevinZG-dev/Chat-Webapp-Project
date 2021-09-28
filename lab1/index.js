2e958f2325baeec037d473f55da92e828355d84d
kevinZG-dev <kevin.zheng@edu.ece.fr>
2021-09-28 16:17:13 +0200

    update nodemon

--- lab1/index.js
+++ lab1/index.js
@@ -1,42 +1,5 @@
-// Import a module
+// ./index.js
 const http = require('http')
-const url = require('url')
-const qs = require('querystring')
-// Define a string constant concatenating strings
-const content = '<!DOCTYPE html>' +
-'<html>' +
-'    <head>' +
-'        <meta charset="utf-8" />' +
-'        <title>ECE AST</title>' +
-'    </head>' + 
-'    <body>' +
-'       <p>Hello World!</p>' +
-'    </body>' +
-'</html>'
-
-const serverHandle = function (req, res) {
-  // Retrieve and print the queryParams
-  const queryParams = qs.parse(url.parse(req.url).query);
-  console.log(queryParams);
-
-  // Retrieve and print the current path
-  const route = url.parse(req.url)
-  const path = route.pathname 
-  const params = qs.parse(route.query)
-
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write(content);
-  res.end();
-}
-
-
-// Declare an http server
-http.createServer(function (callback) {
-
-  callback(req,res);
-
-// Start the server
-})
-
-const server = http.createServer(serverHandle);
+const handles = require('./handles')
+const server = http.createServer(handles.serverHandle);
 server.listen(8080)
