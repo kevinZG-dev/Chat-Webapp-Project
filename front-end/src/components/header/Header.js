ec04518dc2bba0e65eb19fc25a281b236e478ed7
Maxime Attal <attal.maxime.gabriel@gmail.com>
2021-10-18 19:18:18 +0200

    Add main user avatar picture & name in the header

--- front-end/src/components/header/Header.js
+++ front-end/src/components/header/Header.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
+import Avatar from "../channels/Avatar";
 import "./Header.css";
 import logo from "./../../images/logo.png";
+import user5 from "./../../images/user5.jpg";
 
 export default class Header extends Component {
   render(){
@@ -9,8 +11,16 @@ export default class Header extends Component {
         <div className="header_blocks">
           <img src={logo}></img>
         </div>
-        <div className="header_blocks"></div>
-        <div className="header_blocks"></div>
+        <div className="header_blocks">
+          <p>Leonardo Turtle</p>
+        </div>
+        <div className="header_blocks">
+          <div className="user_avatar">
+            <Avatar
+              image={user5}
+            />
+          </div>
+        </div>
       </div>
     );
   }
