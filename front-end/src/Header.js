85e95e9130ab4427979807b166a54d8ecacee4a4
kevinZG-dev <64935494+kevinZG-dev@users.noreply.github.com>
2021-12-13 14:24:06 +0100

    style: header component

--- front-end/src/Header.js
+++ front-end/src/Header.js
@@ -6,12 +6,40 @@ import { useTheme } from '@mui/styles';
 import { IconButton, Link } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import Context from './Context';
+import Button from '@mui/material/Button';
+
 
 const useStyles = (theme) => ({
   header: {
-    padding: theme.spacing(1),
-    backgroundColor: 'rgba(255,255,255,.3)',
+
+    height: "70px",
+
+    backgroundColor: '#1652a1',
     flexShrink: 0,
+    outline: 'none',
+    border: 'none',
+    margin: 8,
+    boxShadow: '3px 3px 3px #0B2951'
+
+  },
+  logoNom: {
+    marginLeft: "50px",
+    height: "30px",
+    width: "120px"
+
+
+  },
+  nom: {
+    marginLeft: 22
+  },
+  content: {
+    position: "absolute",
+    right: "50px",
+    top: "15px",
+    padding: "5px",
+    fontSize: "21px",
+
+
   },
   headerLogIn: {
     backgroundColor: 'red',
@@ -23,6 +51,12 @@ const useStyles = (theme) => ({
     [theme.breakpoints.up('sm')]: {
       display: 'none !important',
     },
+
+    position: "absolute",
+    left: "10px",
+    top: "15px",
+
+
   }
 })
 
@@ -43,6 +77,10 @@ export default function Header({
   }
   return (
     <header css={styles.header}>
+      <div css={styles.logoNom}>
+        <h1 css={styles.nom}>Blabla</h1>
+      </div>
+    
       <IconButton
         color="inherit"
         aria-label="open drawer"
@@ -51,17 +89,20 @@ export default function Header({
       >
         <MenuIcon />
       </IconButton>
-      Header
       {
         oauth ?
-          <span>
+          <span css={styles.content}>
             {oauth.email}
-            <Link onClick={onClickLogout}>logout</Link>
+            <Button
+              sx={{
+                color: '#f1f1f1'
+              }}
+              variant='text' onClick={onClickLogout}>Logout</Button>
           </span>
-        :
-          <span>new user</span>
+          :
+          <span css={styles.content}>new user</span>
       }
-      
+
     </header>
   );
 }
